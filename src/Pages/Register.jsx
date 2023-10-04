a2cca31a87454e13a0de348cee4dbbde5d76d49b
imrulkaisar <imrulkaisar3@gmail.com>
2023-10-04 19:32:03 +0600

    Redirect to homepage after sucessfully register

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,8 +1,17 @@
 import Heading2 from "../Components/Heading2";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import RegisterForm from "../Components/RegisterForm";
+import { useContext } from "react";
+import { UserContext } from "../Context/UserContext";
 
 const Register = () => {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  if (user) {
+    navigate("/");
+  }
+
   return (
     <div className="bg-grayBg py-36 flex justify-center">
       <div className="w-full max-w-xl bg-white p-16 space-y-8">
