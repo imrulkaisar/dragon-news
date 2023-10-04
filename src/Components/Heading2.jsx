22bf15db12c69fe743ddcc27e7c00aeced8f7ba0
imrulkaisar <imrulkaisar3@gmail.com>
2023-10-04 11:55:59 +0600

    Added className prop

--- src/Components/Heading2.jsx
+++ src/Components/Heading2.jsx
@@ -1,5 +1,9 @@
-const Heading2 = ({ children }) => {
-  return <h2 className="text-heading font-semibold text-xl">{children}</h2>;
+const Heading2 = ({ children, className }) => {
+  return (
+    <h2 className={`text-heading font-semibold text-xl ${className}`}>
+      {children}
+    </h2>
+  );
 };
 
 export default Heading2;
